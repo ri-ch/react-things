2ecb8126de1c62971355e3de375da4b01d4f64ad
Richard Crawley <richard.crawley@protonmail.com>
2021-10-22 19:35:46 +0100

    Add linting

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import * as ReactDOM from 'react-dom'
 import MyFunctionalComponent from './component'
 
@@ -7,5 +7,5 @@ const Greet = () =>
         <h1>Hello, world!</h1>
         <MyFunctionalComponent name="Geoff" age={99} />
     </>
-const element = document.getElementById("container")
+const element = document.getElementById('container')
 ReactDOM.render(<Greet/>, element)
--- src/component.tsx
+++ src/component.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 export interface MyComponentProps {
     name: String
     age: number
 }
 
-const MyFunctionalComponent: React.FC<MyComponentProps> = ({name, age}: MyComponentProps) => <div><h2>Age: {age}, Name: {name}</h2></div>
+const MyFunctionalComponent: React.FC<MyComponentProps> = ({ name, age }: MyComponentProps) => <div><h2>Age: {age}, Name: {name}</h2></div>
 export default MyFunctionalComponent
